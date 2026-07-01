95dcccb77a1f5fc80e5fb98f480e31678b30db08
Ayush Chowdhury <ayushchowdhury@Keross.local>
2026-07-01 16:44:25 +0530

    Advanced counter

--- AdvancedCounter/src/components/Decrement.jsx
+++ AdvancedCounter/src/components/Decrement.jsx
@@ -2,11 +2,10 @@ import React from 'react'
 
 const Decrement = ({count,setCount,step}) => {
     const handleDecrement=()=>{
-        count =(count-step<0)?0:count-step;
-        setCount(count)
+        setCount((prev)=>(prev-step<0)?0:prev-step)
     }
   return (
- <button className='bg-blue-400 rounded-2xl outline-0 w-[155px] h-[60px] active:scale-85' onClick={handleDecrement}>Decrement</button>  )
+ <button disabled={count<=0} className='bg-blue-400 rounded-2xl outline-0 w-[155px] h-[60px] active:scale-85 disabled:bg-gray-400 scale-none cursor-not-allowed' onClick={handleDecrement}>Decrement</button>  )
 }
 
 export default Decrement
